91da4fb9918f642c9379e88066791a49dc248992
dhunganabimal <dhunganabimal07.com>
2025-04-22 09:16:49 +0545

    Updated Expenditure Dashboard with its servlet

--- src/main/java/org.example.bimal.servlets/ExpServlet.java
+++ src/main/java/org.example.bimal.servlets/ExpServlet.java
@@ -1,4 +1,60 @@
 package org.example.bimal.servlets;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+public class ExpServlet extends HttpServlet{
+    private static final String DB_URL = "jdbc:mysql://localhost/login_db?useSSL=false";
+    private static final String DB_USER = "root1";
+    private static final String DB_PASSWORD = "rootpass";
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+        String date = req.getParameter("edate");
+        String time = req.getParameter("etime");
+        String dateTime = date + " " + time;
+        String incomeSource=req.getParameter("esource");
+        String amount=req.getParameter("eamount");
+//        PrintWriter out=res.getWriter();
+//        out.println(date);
+//        out.print(time);
+//        out.println(dateTime);
+//        out.println(incomeSource);
+//        out.println(amount);
+        RequestDispatcher dispatcher=null;
+        Connection conn;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver") ;
+            String url = "jdbc:mysql://localhost/login_db?useSSL=false";
+            String user ="root1";
+            String pass="rootpass";
+            conn = DriverManager.getConnection(url,user,pass);
+            String query="Insert into expData (edate,esource,eamount) values(?,?,?)";
+            PreparedStatement stmt=conn.prepareStatement(query);
+            stmt.setString(1,dateTime);
+            stmt.setString(2,incomeSource);
+            stmt.setString(3,amount);
+            int rowCount=stmt.executeUpdate();
+            dispatcher=req.getRequestDispatcher("exp.jsp");
+            if(rowCount>0){
+                req.setAttribute("status","success");
 
-public class ExpServlet {
+            }
+            else{
+                req.setAttribute("status","failed");
+            }
+            dispatcher.forward(req,res);
+
+            conn.close();
+        }
+        catch(ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+
+    }
 }
+
